52490dc38097a3205eab5813f099630057606fe4
Rajasri <dkraji09karthick@gmail.com>
2025-09-21 21:50:29 +0530

    Added NodeMongo, NodePostgres, and React folders

---
+++ NodeMongo/Myapp/index.js
@@ -0,0 +1,11 @@
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+.then(() => console.log("✅ MongoDB Connected Successfully"))
+.catch(err => console.error("❌ MongoDB Connection Failed:", err));
